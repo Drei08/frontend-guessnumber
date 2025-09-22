38d729ee5274b5d263c93632e41ed6139454f48b
Drei08 <andrei_rheivjarth@hotmail.com>
2025-09-22 20:52:40 -0300

    feat: update login.html

---
+++ src/js/backup.js
@@ -0,0 +1,90 @@
+//LOGIN
+
+const login = async (login) => {
+
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  localStorage.removeItem("user");
+
+  const bodyString = JSON.stringify({
+      email,
+      password,
+    })
+
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/auth`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: bodyString,
+  });
+
+  const responseMessage = await res.json();
+  console.log(responseMessage);
+
+  if(res?.status === 200) {
+    Toastify({
+      text: responseMessage?.message ?? "Login com Sucesso!",
+      className: "info",
+    }).showToast();
+
+    setTimeout(() => {
+      localStorage.setItem("user", bodyString);
+      window.location.href = "/";
+    }, 1000);
+
+  } 
+  else {
+    Toastify({
+      text: responseMessage?.message ?? "Dados Errados!",
+      className: "toast__error",
+    }).showToast();
+  }
+};
+
+window.login = login;
+
+
+
+
+//MAIN
+
+const tagVersiculos = document.getElementById("versiculos");
+console.log("META", import.meta.env);
+
+const fetchData = async () => {
+  console.log("META", import.meta.env);
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/versiculos`);
+  const data = await res.json();
+  
+  tagVersiculos.textContent = data.versiculos.texto;
+  
+  console.log(data.versiculos.texto);
+};
+
+window.onload =  async () => {
+  if (!localStorage.getItem("user")) {  
+    window.location.href = "/login.html";
+  }
+
+  const bodyString = localStorage.getItem("user");
+
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/auth`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: bodyString,
+  });
+
+  if(res?.status !== 200) {
+    window.location.href = "/login";
+  }
+
+};
+
+window.fetchData = fetchData;
