0e31e7585d4da7e0d3a810b2883961c366391190
Drei08 <andrei_rheivjarth@hotmail.com>
2025-09-24 07:50:26 -0300

    feat: update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,47 @@
-const criarConta = (event) => {
+const criarConta = async (event) => {
+
   event.preventDefault();
-  window.location.href = "/";
+
+  const formData = new FormData(event.target);
+  const username = formData.get("username");
+  const password = formData.get("password");
+
+  localStorage.removeItem("user");
+
+  const bodyString = JSON.stringify({
+      username,
+      password,
+    })
+
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/authsign`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: bodyString,
+  });
+
+  const responseMessage = await res.json();
+  console.log(responseMessage);
+
+  if(res?.status === 200) {
+    Toastify({
+      text: responseMessage?.message ?? "Login com Sucesso!",
+      className: "info",
+    }).showToast();
+
+    setTimeout(() => {
+      localStorage.setItem("user", bodyString);
+      window.location.href = "/";
+    }, 1000);
+
+  } 
+  else {
+    Toastify({
+      text: responseMessage?.message ?? "Dados Errados!",
+      className: "toast__error",
+    }).showToast();
+  }
 };
 
 window.criarConta = criarConta;
